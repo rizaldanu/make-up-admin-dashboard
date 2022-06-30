377c9c7f2fa1922b82c9d235697fd380798f516f
Rizal Danu <gwyllgi.dollars@gmail.com>
2022-07-01 03:49:46 +0800

    update laporan

--- laporan.php
+++ laporan.php
@@ -1,7 +1,7 @@
 <?php
-    session_start();
-    if(empty($_SESSION['username'])){
-    header("location:login.php");  
+session_start();
+if (empty($_SESSION['username'])) {
+  header("location:login.php");
 }
 ?>
 <?php
@@ -12,33 +12,71 @@ include "navigation.php";
 
 <!DOCTYPE html>
 <html>
+
 <head>
   <title>Laporan | Ochii Make-Up</title>
 </head>
 
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Laporan</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Laporan</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-    <div class="d-flex justify-content-center">
-      <img src="dist/img/ochii-logo.jpg"></img>
-    </div></br>
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">Laporan</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active">Laporan</li>
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
   </div>
+  <!-- /.content-header -->
+  <div class="d-flex justify-content-center">
+    <div class="container-fluid">
+      <div class="row justify-content-center">
+        <div class="col-lg-3 col-6">
+          <!-- small card -->
+          <div class="small-box bg-success">
+            <div class="inner">
+              <h3>63</h3>
+
+              <p>Pesanan Selesai</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-stats-bars"></i>
+            </div>
+            <a href="#" class="small-box-footer">
+              More info <i class="fas fa-arrow-circle-right"></i>
+            </a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-6">
+          <!-- small card -->
+          <div class="small-box bg-danger">
+            <div class="inner">
+              <h3>65</h3>
+
+              <p>Pesanan Batal</p>
+            </div>
+            <div class="icon">
+              <i class="fas fa-chart-pie"></i>
+            </div>
+            <a href="#" class="small-box-footer">
+              More info <i class="fas fa-arrow-circle-right"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 </html>
 
 <?php
